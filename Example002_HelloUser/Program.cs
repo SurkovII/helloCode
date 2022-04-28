9c16379e2152773c18ca9f1a485d083b4cd6ec01
Ivan <Havana8888888@gmail.com>
2022-04-28 21:54:13 +0300

    add Example 003 Sum

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите ваше имя ");
+﻿Console.WriteLine("Введите ваше имя ");
+//WriteLine перенос на новую строку
 string username = Console.ReadLine();
 Console.Write("Привет, ");
+//Write в одну строку
 Console.Write(username);
