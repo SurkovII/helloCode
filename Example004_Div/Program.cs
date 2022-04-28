eb81a16a1898b5f6d1d8e6f3d2c86397dd2795ed
Ivan <Havana8888888@gmail.com>
2022-04-28 22:57:39 +0300

    add random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,7 +1,11 @@
 ﻿double numberA = 12;
 double numberB = 5;
+//дробные числа
 
 int numberC = 12;
 int numberD = 5;
+//int целые числа
+//string строка
+//bool истина и ложь
 Console.WriteLine(numberA/numberB);
 Console.WriteLine(numberC/numberD);
